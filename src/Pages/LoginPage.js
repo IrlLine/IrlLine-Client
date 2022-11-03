46884832f52859da1919d28394e7bb40ad138749
svenskapojkarna <hytonenaleksi@gmail.com>
2022-11-04 00:03:49 +0200

    Development

---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,11 @@
+import '../Styles/LoginPage.css';
+
+function LoginPage() {
+  return (
+    <div className='LoginPage'>
+      <p className='text'>Something</p>
+    </div>
+  );
+}
+
+export default LoginPage;
