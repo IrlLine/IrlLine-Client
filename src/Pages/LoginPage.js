678602282e0a68d4f132ba9bdfdae0d13b86329d
svenskapojkarna <hytonenaleksi@gmail.com>
2022-11-06 00:20:23 +0200

    Development

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,7 +1,36 @@
-import '../Styles/LoginPage.css';
+import {useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import '../Styles/LoginPage.css'
 
 function LoginPage()
 {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [correctUsernameInput, setCorrectUsernameInput] = useState(true)
+  const [correctPasswordInput, setCorrectPasswordInput] = useState(true)
+
+  const usernameChangeHandler = event => {
+    setUsername(event.target.value)
+  }
+
+  const passwordChangeHandler = event => {
+    setPassword(event.target.value)
+  }
+
+  let navigate = useNavigate(); 
+  const loginhandler = event =>{  
+    event.preventDefault()
+    console.log('Username: '+ username + ', Password: ' + password)
+    if (username != '' && password != '') {
+      navigate("/testi");
+    }
+    else {
+      setCorrectUsernameInput(username != '')
+      setCorrectPasswordInput(password != '')
+    }
+  }
+
   return (
     <div className='LoginPage'>
       <form className="Auth-form">
@@ -11,20 +40,22 @@ function LoginPage()
             <label>Username:</label>
             <input
               type="text"
-              className="form-control mt-1"
+              className={correctUsernameInput ? "form-control mt-1" : "form-control mt-1 IncorrectInput"}
               placeholder="Enter username"
+              onChange={usernameChangeHandler}
             />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
             <input
               type="password"
-              className="form-control mt-1"
+              className={correctPasswordInput ? "form-control mt-1" : "form-control mt-1 IncorrectInput"}
               placeholder="Enter password"
+              onChange={passwordChangeHandler}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" onClick={loginhandler}>
               Login
             </button>
           </div>
@@ -34,4 +65,4 @@ function LoginPage()
   );
 }
 
-export default LoginPage;
+export default LoginPage
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './Styles/index.css';
 import LoginPage from './Pages/LoginPage';
+import Dashboard from './Pages/Dashboard';
 
-function LoginPageWrapper(){
-  return <LoginPage />
-}
-
-function render(){
-  ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
-      <Route exact path="/" component={LoginPageWrapper} />
-  </BrowserRouter>, 
-  document.getElementById('root'));
-}
-render();
+    <Routes>
+      <Route exact path="/" element={<LoginPage />} />
+      <Route exact path="/testi" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+);
